5c6f331695925cc0d766b4a8b25552e2bf8c53a0
SimonBuusJensen <simonbuusjensen@hotmail.com>
2019-09-17 22:11:21 +0200

    Added an imageView which displays the current chord to be played

--- app/src/main/java/com/example/simonsguitarapp/MainActivity.kt
+++ app/src/main/java/com/example/simonsguitarapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.util.TypedValue
+import kotlin.collections.HashMap
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,28 +21,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var timer: Timer
+    private lateinit var timerThread : Thread
+
     private val timeBetweenChordShuffleInMillis: Long = 3000
     private var timerState = TimerState.Paused
 
-    private var majorChords = setOf<String>("A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#")
-    private var minorChords = setOf<String>("Am", "Bbm", "Bm", "Cm", "C#m", "Dm", "Ebm", "Em", "Fm", "F#m", "Gm", "G#m")
+    private val chordStringToChordImage : HashMap<String, Int> = hashMapOf(
+        "A" to R.drawable.a,
+        "B" to R.drawable.b,
+        "C" to R.drawable.c,
+        "D" to R.drawable.d,
+        "E" to R.drawable.e,
+        "F" to R.drawable.f,
+        "G" to R.drawable.g,
+        "Am" to R.drawable.am,
+        "Bm" to R.drawable.bm,
+        "Cm" to R.drawable.cm,
+        "Dm" to R.drawable.dm,
+        "Em" to R.drawable.em,
+        "Fm" to R.drawable.fm,
+        "Gm" to R.drawable.gm
+    )
+
+    //  "Bb", "C#",  "Eb", "F#", "G#"
+    private var majorChords = setOf<String>("A", "B", "C", "D", "E", "F", "G")
+
+    // "Bbm", "C#m", "Ebm", "F#m", "G#m"
+    private var minorChords = setOf<String>("Am", "Bm", "Cm", "Dm", "Em", "Fm", "Gm")
     private var dom7MajorChords = setOf<String>("A7", "Bb7", "B7", "C7", "C#7", "D7", "Eb7", "E7", "F7", "F#7", "G7", "G#7")
     private var dom7MinorChords = setOf<String>("Am7", "Bbm7", "Bm7", "Cm7", "C#m7", "Dm7", "Ebm7", "Em7", "Fm7", "F#m7", "Gm7", "G#m7")
+    private var allChords = mutableListOf<String>()
+
     private var oneCheckBoxIsChecked : Boolean = true
 
     private var currentDisplayedChord: String = ""
     private var nextChordToDisplay: String = ""
 
-    private var allChords = mutableListOf<String>()
-    private lateinit var timerThread : Thread
-
-    fun initializeView() {
+    private fun initializeView() {
         majorChordsCheckBox.isChecked = true
         allChords.addAll(majorChords)
         chordTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, getResources().getDimension(R.dimen.text_font_size))
     }
 
-    fun displayNextChord() {
+    private fun displayNextChord() {
         if (allChords.size > 0) {
             while (currentDisplayedChord == nextChordToDisplay) {
                 nextChordToDisplay = allChords.random()
@@ -51,13 +73,15 @@ class MainActivity : AppCompatActivity() {
             chordTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, getResources().getDimension(R.dimen.chord_font_size))
             chordTextView.text = nextChordToDisplay
             currentDisplayedChord = nextChordToDisplay
+
+            chordImageView.setImageResource(chordStringToChordImage.get(currentDisplayedChord)!!)
         }
         else {
             chordTextView.text = getString(R.string.no_chords)
         }
     }
 
-    fun noChordCheckBoxChecked() {
+    private fun noChordCheckBoxChecked() {
         oneCheckBoxIsChecked = false
         System.out.println("Setting text size to " + getResources().getDimension(R.dimen.text_font_size))
         chordTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, getResources().getDimension(R.dimen.text_font_size))
@@ -70,9 +94,11 @@ class MainActivity : AppCompatActivity() {
         startPauseButton.text = getString(R.string.start)
         startPauseButton.isClickable = false
         timerState = TimerState.Paused
+
+        chordImageView.setImageResource(R.drawable.am)
     }
 
-    fun chordCheckBoxChecked(chords: Set<String>) {
+    private fun chordCheckBoxChecked(chords: Set<String>) {
         allChords.addAll(chords)
         startPauseButton.isClickable = true
 
@@ -83,7 +109,7 @@ class MainActivity : AppCompatActivity() {
         oneCheckBoxIsChecked = true
     }
 
-    fun initializeOnCheckBoxListeners() {
+    private fun initializeOnCheckBoxListeners() {
 
         minorChordsCheckBox.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -146,8 +172,6 @@ class MainActivity : AppCompatActivity() {
         initializeView()
         initializeOnCheckBoxListeners()
 
-        System.out.println("Initial text size " + chordTextView.textSize)
-
         startPauseButton.setOnClickListener(object: View.OnClickListener {
             override fun onClick(v: View?) {
 
@@ -180,7 +204,7 @@ class MainActivity : AppCompatActivity() {
                     startPauseButton.text = getString(R.string.start)
                     timerState = TimerState.Paused
                     timerThread.interrupt()
-                    //System.out.println("Setting text size to " + getResources().getDimension(R.dimen.text_font_size))
+
                     chordTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, getResources().getDimension(R.dimen.text_font_size))
                     chordTextView.text = getString(R.string.start_practicing)
                 }
